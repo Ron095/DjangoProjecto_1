d332880d9dec862a12b549ffc2cdc7c6c1679784
Ronald Rodriguez <ronrf095@hotmail.com>
2025-02-20 18:57:31 -0500

    change home

--- djangoProject_01/urls.py
+++ djangoProject_01/urls.py
@@ -20,7 +20,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('/', views.home, name='home'),
 
     path('signup/', views.signup, name='signup'),
 
